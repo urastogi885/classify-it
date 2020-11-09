851c67ce1fdcb2152e3fdfa9d7946034ca5c2039
Umang Rastogi <umang.rastogi96@gmail.com>
2020-11-09 17:42:18 -0500

    [knn_classifier.m] Generalize for k-neighbors

--- Code/knn_classifier.m
+++ Code/knn_classifier.m
@@ -1,18 +1,19 @@
-function accuracy = knn_classifier(test_data, train_data, label_test, label_train, len_test, len_train)
+function accuracy = knn_classifier(k, test_data, train_data, label_test, label_train, len_test, len_train)
     % Predict the output for each test case and verify the correctness of your classifier's prediction
-    prediction = zeros(len_test, 1);
+    k = str2double(k);
+    dist = zeros(len_train, 1);
+    min_labels = zeros(k, 1);
     accuracy = 0.0;
     for i=1:len_test
-        min_dist = (test_data(:,i) - train_data(:,1))'*(test_data(:,i) - train_data(:,1));
         for j = 1:len_train
-            dist = (test_data(:,i) - train_data(:,j))'*(test_data(:,i) - train_data(:,j));
-            if dist <= min_dist
-               min_dist = dist;
-               prediction(i) = label_train(j);
-            end
+            dist(j, 1) = (test_data(:,i) - train_data(:,j))'*(test_data(:,i) - train_data(:,j));
+        end
+        [~, indices] = sort(dist);
+        for m=1:k
+            min_labels(m) = label_train(indices(m));
         end
         % Evaluate the prediction of your classifier for each test case
-        if prediction(i) == label_test(i)
+        if mode(min_labels) == label_test(i)
             accuracy = accuracy + 1;
         end
     end
