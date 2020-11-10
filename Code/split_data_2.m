0a0eb64267def81c002c3193808e948658383482
Umang Rastogi <umang.rastogi96@gmail.com>
2020-11-10 14:02:19 -0500

    [split_data_2.m] Fix data splitting

--- Code/split_data_2.m
+++ Code/split_data_2.m
@@ -10,29 +10,31 @@ function [train_data, test_data, dim, total_classes, len_train_class, len_test_c
     % No. of samples in the testing and training data
     len_train = len_train_class*total_classes;
     len_test = len_test_class*total_classes;
+    neutral = zeros(dim, size_data(3)/3);
+    facial_exp = zeros(dim, size_data(3)/3);
     % Testing and training data sets
     train_data = zeros(dim, len_train);
     test_data = zeros(dim, len_test);
     % Define labels for data sets
     label_train = zeros(len_train, 1);
     label_test = zeros(len_test, 1);
-    % Reshape the data set and add images to each dataset
-    for i=1:2*size_data(3)/3
-       if mod(i,3) == 2
-          train_data(:, floor(i/3)+1) = reshape(face(:,:,i), [dim,1]);
-          label_train(floor(i/3)+1) = 1;
-       elseif mod(i,3) == 1
-          train_data(:, floor(i/3)+2) = reshape(face(:,:,i), [dim,1]);
-          label_train(floor(i/3)+1) = 2;
-       end
+    % Reshape the data set
+    for i=1:size_data(3)/3
+        neutral(:,i) = reshape(face(:,:,3*i-2), [dim,1]);
+        facial_exp(:,i) = reshape(face(:,:,3*i-1), [dim,1]);
     end
-    for i=2*size_data(3)/3+1:size_data(3)
-       if mod(i,3) == 2
-          test_data(:, floor(i/400)) = reshape(face(:,:,i), [dim,1]);
-          label_test(floor(i/400)) = 1;
-       elseif mod(i,3) == 1
-          test_data(:, floor(i/400)) = reshape(face(:,:,i), [dim,1]);
-          label_test(floor(i/400)) = 2;
-       end
-    end
+
+    % Add images to the training data set
+    train_data(:,1:len_train_class) = neutral(:,1:len_train_class);
+    train_data(:,len_train_class+1:len_train) = facial_exp(:,1:len_train_class);
+    % Add labels for the training data set
+    label_train(1:len_train_class) = 1;
+    label_train(len_train_class+1:len_train) = 2;
+
+    % Add images to the testing data set
+    test_data(:,1:len_test_class) = neutral(:,len_train_class+1:end);
+    test_data(:,len_test_class+1:len_test) = facial_exp(:,len_train_class+1:end);
+    % Add labels for the testing data set
+    label_test(1:len_test_class) = 1;
+    label_test(len_test_class+1:len_test) = 2;
 end
